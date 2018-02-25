3bd05abe41e3c401a4ac640e744f3c8c571e9389
wa1tnr <wa1tnr@users.noreply.github.com>
2018-02-25 22:00:22 +0000

    modified:   adafruit_st7565-ex-small.py edits.

--- Adafruit_CircuitPython_ST7565-ex-small/adafruit_st7565-ex-small.py
+++ Adafruit_CircuitPython_ST7565-ex-small/adafruit_st7565-ex-small.py
@@ -46,9 +46,26 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_fictional_ST7565.
 SET_VOLUME_FIRST        = const(0x81)  # enhance gLCD contrast
 SET_VOLUME_SECOND       = const(0x00)  # contrast, follow-on value
                                        # ex. 0x81, then 0x00 | (0x1d & 0x3f)
+CMD_RMW                 = const(0xE0)
+CMD_RMW_CLEAR           = const(0xEE)
+CMD_INTERNAL_RESET      = const(0xE2)
 
 # Early versions of the present program will kludge hexadecimal values
 # directly into the code, and will not use these constants.
+
+CMD_NOP                 = const(0xE3)
+CMD_TEST                = const(0xF0)
+#pylint: enable-msg=bad-whitespace
+
+
+class _ST7565:
+    """Base class for ST7565 display driver"""
+    #pylint: disable-msg=too-many-arguments
+    #pylint: disable-msg=too-many-instance-attributes
+    def __init__(self, framebuffer, width, height, external_vcc, reset):
+        self.framebuf = framebuffer
+        self.fill = self.framebuf.fill
+
 
  //////////////////////// intrusion ////////////////////////////
 
@@ -56,23 +73,6 @@ SET_VOLUME_SECOND       = const(0x00)  # contrast, follow-on value
  quick visual means to identify a large block of inserted code.
 
  //////////////////////// intrusion ////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
