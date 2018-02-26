1a89b1f2a4d7526f724e076c347ca05c90382314
wa1tnr <wa1tnr@users.noreply.github.com>
2018-02-26 23:38:50 +0000

    modified:   simpletest.py

--- Adafruit_CircuitPython_ST7565-ex-small/examples/simpletest.py
+++ Adafruit_CircuitPython_ST7565-ex-small/examples/simpletest.py
@@ -1,35 +1,120 @@
-# Basic example of clearing and drawing pixels on a SSD1306 OLED display.
+# Basic example of clearing and drawing pixels on a ST7565 graphic LCD display.
 # This example and library is meant to work with Adafruit CircuitPython API.
 # Author: Tony DiCola
+# Modified: Christopher W Hafey, wa1tnr - February 2018
 # License: Public Domain
 
 # Import all board pins.
-from board import SCL, SDA
+import board
 import busio
+import digitalio
+import time
 
-# Import the SSD1306 module.
-import adafruit_ssd1306
+# Import the ST7565 module.
+import adafruit_st7565
 
+# Setup the port pins
+data = board.MOSI
+clk  = board.SCK
 
-# Create the I2C interface.
-i2c = busio.I2C(SCL, SDA)
+backlight = digitalio.DigitalInOut(board.A4) # optional - do not distribute
+a0        = digitalio.DigitalInOut(board.D9)
+rst       = digitalio.DigitalInOut(board.D6)
+cs        = digitalio.DigitalInOut(board.D5)
 
-# Create the SSD1306 OLED class.
+backlight.switch_to_output(value=True,  drive_mode=digitalio.DriveMode.PUSH_PULL) # optional
+
+a0.switch_to_output(value       =False, drive_mode=digitalio.DriveMode.PUSH_PULL)
+rst.switch_to_output(value      =True,  drive_mode=digitalio.DriveMode.PUSH_PULL)
+cs.switch_to_output(value       =True,  drive_mode=digitalio.DriveMode.PUSH_PULL)
+
+# Create the SPI interface.
+spi = busio.SPI(clk, MOSI=data)
+
+# Create the ST7565 gLCD class.
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-# Alternatively you can change the I2C address of the device with an addr parameter:
-#display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
+display = adafruit_st7565.ST7565_SPI(128, 64, spi, a0, rst, cs, external_vcc=False, baudrate=2000000, polarity=0, phase=0)
 
-# Clear the display.  Always call show after changing pixels to make the display
-# update visible!
-display.fill(0)
-display.show()
+display.fill(1)
+
+# display.show()
 
 # Set a pixel in the origin 0,0 position.
-display.pixel(0, 0, 1)
+# display.pixel(0, 0, 1)
 # Set a pixel in the middle 64, 16 position.
-display.pixel(64, 16, 1)
+# display.pixel(64, 16, 1)
 # Set a pixel in the opposite 127, 31 position.
-display.pixel(127, 31, 1)
+# display.pixel(127, 31, 1)
+# display.show()
+
+def char_out():
+    global cmd, mbytes
+    for p in range(0,6):
+        cmd = mbytes[p]
+        display.write_data_out(cmd)
+
+def write_short_phrase_to_lcd():
+    global mbytes
+    mbytes = bytearray(b'\x3e\x48\x88\x48\x3e\x00')  # A
+    char_out()
+    mbytes = bytearray(b'\xfe\x82\x82\x82\x7c\x00')  # D 
+    char_out()
+    mbytes = bytearray(b'\x3e\x48\x88\x48\x3e\x00')  # A
+    char_out()
+    mbytes = bytearray(b'\xfe\x90\x90\x90\x80\x00')  # F
+    char_out()
+    mbytes = bytearray(b'\xfe\x90\x98\x94\x62\x00')  # R
+    char_out()
+    mbytes = bytearray(b'\xfc\x02\x02\x02\xfc\x00')  # U
+    char_out()
+    mbytes = bytearray(b'\x00\x82\xfe\x82\x00\x00')  # I
+    char_out()
+    mbytes = bytearray(b'\xc0\x80\xfe\x80\xc0\x00')  # T
+    char_out()
+
+"""
+    mbytes = bytearray(b'\xfc\x02\x1c\x02\xfc\x00') # W - deshipu driver leads this idea
+    char_out()
+    mbytes = bytearray(b'\x3e\x48\x88\x48\x3e\x00') # A
+    char_out()
+    mbytes = bytearray(b'\x00\x42\xfe\x02\x00\x00') # 1
+    char_out()
+    mbytes = bytearray(b'\xc0\x80\xfe\x80\xc0\x00') # T
+    char_out()
+    mbytes = bytearray(b'\xfe\x20\x10\x08\xfe\x00') # N
+    char_out()
+    mbytes = bytearray(b'\xfe\x90\x98\x94\x62\x00') # R
+    char_out()
+    mbytes = bytearray(b'\x10\x10\x10\x10\x10\x00') # dash
+    char_out()
+    mbytes = bytearray(b'\x82\x84\x88\x90\xe0\x00') # 7
+    char_out()
+"""
+
+
+
+def disp_text_geom():
+    global cmd
+    column =  32 # must be zero or a multiple of 16
+    icol   = column + 16
+    cursor_right = (icol // 16) - 1
+    cmd = (0x10 | cursor_right) ; display.write_cmd(cmd) # upper x positional byte
+    cmd = (0x00 | 3)    ; display.write_cmd(cmd) # lower x positional byte - fine tuning
+    cmd = (0xb0 | 0)    ; display.write_cmd(cmd) # only  y positional byte - page 0 is middle row
+    cmd = 0xe0          ; display.write_cmd(cmd)
+    # write data here
+
+
+mbytes = bytearray(b'\x10\x03\xb2\xe0') # 7
+for p in range(0,4):
+    cmd = mbytes[p]
+    display.write_cmd(cmd)
+display.fill(0xff)
 display.show()
+
+time.sleep(8.1)
+
+disp_text_geom()
+write_short_phrase_to_lcd()
+
