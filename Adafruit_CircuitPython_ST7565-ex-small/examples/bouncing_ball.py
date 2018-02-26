33ac267783508b0c41b55bcf4eea8707aeed5690
wa1tnr <wa1tnr@users.noreply.github.com>
2018-02-26 22:53:42 +0000

    modified:   bouncing_ball.py Working minimally.  Bouncing ball stays in one row only.  Framebuffer geometry.

--- Adafruit_CircuitPython_ST7565-ex-small/examples/bouncing_ball.py
+++ Adafruit_CircuitPython_ST7565-ex-small/examples/bouncing_ball.py
@@ -1,15 +1,30 @@
 import board
 import busio
-import adafruit_ssd1306
+import digitalio
+import adafruit_st7565
 
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+print("jake calico  feldspar cranshow rotashow CENOPIC")
 
-# Create the SSD1306 OLED class.
+backlight = digitalio.DigitalInOut(board.A4)
+data = board.MOSI   # Pin connected to backpack DAT/data.
+clk  = board.SCK    # Pin connected to backpack CLK.
+a0   = digitalio.DigitalInOut(board.D9)
+rst  = digitalio.DigitalInOut(board.D6)
+cs   = digitalio.DigitalInOut(board.D5)
+backlight.switch_to_output(value=True, drive_mode=digitalio.DriveMode.PUSH_PULL)
+a0.switch_to_output(value  =True, drive_mode=digitalio.DriveMode.PUSH_PULL)
+rst.switch_to_output(value =True, drive_mode=digitalio.DriveMode.PUSH_PULL)
+cs.switch_to_output(value  =True, drive_mode=digitalio.DriveMode.PUSH_PULL)
+
+# Create the SPI interface.
+spi = busio.SPI(clk, MOSI=data)
+
+# Create the ST7565 gLCD class.
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
-oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
+display = adafruit_st7565.ST7565_SPI(128, 64, spi, a0, rst, cs, external_vcc=False, baudrate=2000000, polarity=0, phase=0)
 
 # Helper function to draw a circle from a given position with a given radius
 # This is an implementation of the midpoint circle algorithm,
@@ -21,14 +36,14 @@ def draw_circle(xpos0, ypos0, rad, col=1):
     dy = 1
     err = dx - (rad << 1)
     while x >= y:
-        oled.pixel(xpos0 + x, ypos0 + y, col)
-        oled.pixel(xpos0 + y, ypos0 + x, col)
-        oled.pixel(xpos0 - y, ypos0 + x, col)
-        oled.pixel(xpos0 - x, ypos0 + y, col)
-        oled.pixel(xpos0 - x, ypos0 - y, col)
-        oled.pixel(xpos0 - y, ypos0 - x, col)
-        oled.pixel(xpos0 + y, ypos0 - x, col)
-        oled.pixel(xpos0 + x, ypos0 - y, col)
+        display.pixel(xpos0 + x, ypos0 + y, col)
+        display.pixel(xpos0 + y, ypos0 + x, col)
+        display.pixel(xpos0 - y, ypos0 + x, col)
+        display.pixel(xpos0 - x, ypos0 + y, col)
+        display.pixel(xpos0 - x, ypos0 - y, col)
+        display.pixel(xpos0 - y, ypos0 - x, col)
+        display.pixel(xpos0 + y, ypos0 - x, col)
+        display.pixel(xpos0 + x, ypos0 - y, col)
         if err <= 0:
             y += 1
             err += dy
@@ -48,15 +63,15 @@ y_inc = 1
 radius = 8
 
 # start with a blank screen
-oled.fill(0)
+display.fill(0)
 # we just blanked the framebuffer. to push the framebuffer onto the display, we call show()
-oled.show()
+display.show()
 while True:
     # undraw the previous circle
     draw_circle(center_x, center_y, radius, col=0)
 
     # if bouncing off right
-    if center_x + radius >= oled.width:
+    if center_x + radius >= display.width:
         # start moving to the left
         x_inc = -1
     # if bouncing off left
@@ -65,7 +80,7 @@ while True:
         x_inc = 1
 
     # if bouncing off top
-    if center_y + radius >= oled.height:
+    if center_y + radius >= display.height:
         # start moving down
         y_inc = -1
     # if bouncing off bottom
@@ -80,4 +95,6 @@ while True:
     # draw the new circle
     draw_circle(center_x, center_y, radius)
     # show all the changes we just made
-    oled.show()
+    display.show()
+
+
